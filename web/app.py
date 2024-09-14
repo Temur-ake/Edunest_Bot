b2e65fb96634ee0d77869a76a599f43005c14344
Temur-ake <kozimovt0@gmail.com>
2024-09-14 21:36:55 +0500

    commit

--- web/app.py
+++ web/app.py
@@ -2,22 +2,20 @@ import uvicorn
 from starlette.applications import Starlette
 from starlette.middleware import Middleware
 from starlette.middleware.sessions import SessionMiddleware
-import itsdangerous
 from starlette_admin.contrib.sqla import Admin, ModelView
-
-from db.models import Contact, engine, News
-from web.login import UsernameAndPasswordProvider
+import db
+from .login import UsernameAndPasswordProvider
 
 app = Starlette()
 
-admin = Admin(engine, title="Example: SQLAlchemy",
+admin = Admin(db.models.engine, title="Example: SQLAlchemy",
               base_url='/',
               auth_provider=UsernameAndPasswordProvider(),
               middlewares=[Middleware(SessionMiddleware, secret_key="qewrerthytju4")],
               )
 
-admin.add_view(ModelView(Contact, icon='fas fa-contacts'))
-admin.add_view(ModelView(News, icon='fas fa-news'))
+admin.add_view(ModelView(db.models.Contact, icon='fas fa-contacts'))
+admin.add_view(ModelView(db.models.News, icon='fas fa-news'))
 
 admin.mount_to(app)
 if __name__ == '__main__':
