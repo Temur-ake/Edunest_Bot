7a9435e668869b9688cdde1f94983c0664871666
Temur-ake <kozimovt0@gmail.com>
2024-09-14 19:03:41 +0500

    commit

--- db/models.py
+++ db/models.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import DeclarativeBase, declared_attr, Session
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
-engine = create_engine(f'postgresql+psycopg2://postgres:1@localhost:5432/postgres')
+engine = create_engine(f'postgresql+psycopg2://postgres:1@localhost:5432/postgres_temur')
 session = Session(bind=engine)
 
 
